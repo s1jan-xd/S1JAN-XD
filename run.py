4ddb1c688daaa64fdb3068dcf8e27c10245b5c07
s1jan-xd <sijanxd1@gmail.com>
2025-01-14 18:20:49 +0545

    run.py

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ os.system('clear')
 os.system("git pull")
 b = platform.architecture()[0]
 if b == '64bit':
-    import MX1.cpython-404.so
+    import MX1
     
 elif b == '32bit':
-    import MX2.cpython-404.so
+    import MX2
 
